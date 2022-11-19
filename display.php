10ef08651d77816544e245e0cdf2cdacc8a0e830
Volodymyr Kolesnikov <114644546+Fewch1@users.noreply.github.com>
2022-11-19 14:27:42 +0100

    Update display.php

--- display.php
+++ display.php
@@ -2,5 +2,5 @@
 $name = $_POST["firstname"];
 $surname = $_POST["lastname"];
 $usercity = $_POST["user_city"];
-echo "Hello, <b>".$name . " " . $surname . "</b> " " from " "<b>". $user_city ."</b> .";
+echo "Hello, <b>".$name . " " . $surname . " " . $usercity . "</b> .";
 ?>
