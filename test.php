e752ec11c62d98f114004de37366c77a60f746fd
Volodymyr Kolesnikov <114644546+Fewch1@users.noreply.github.com>
2022-11-19 14:21:12 +0100

    Update test.php

--- test.php
+++ test.php
@@ -6,8 +6,18 @@
 <body>
 <h2>Contact form:</h2>
 <form action="display.php" method="POST">
-<p>Your name: <input type="text" name="firstname" value="Firstname" /></p>
-<input type="text" name="lastname" value="Lastname" />
+<p>Your name: <input type="text" name="firstname" value="Firstname" /> <input type="text" name="lastname" value="Lastname" /> </p>
+<p><select name="user_city">
+  <optgroup label="Slovakia">
+    <option value="7">Bratislava</option>
+    <option value="8">Košice</option>
+  </optgroup>
+  <optgroup label="Ukraina">
+    <option value="4">Kyjev</option>
+    <option value="5">Lviv</option>
+    <option value="6">Odesa</option>
+  </optgroup>
+  </select></p>
 <input type="submit" value="Submit form">
 </form>
 </body>
