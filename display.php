431eb6b82feb0d50fafb784df8e6c979d9211177
Volodymyr Kolesnikov <114644546+Fewch1@users.noreply.github.com>
2022-11-19 13:49:34 +0100

    Update display.php

--- display.php
+++ display.php
@@ -1,5 +1,8 @@
-<?php
-$name = $_POST["firstname"];
-$surname = $_POST["lastname"];
-echo "Your name: <b>".$name . " " . $surname . "</b>";
-?>
+<html>
+ <head>
+  <title>Тестируем PHP</title>
+ </head>
+ <body>
+ <?php echo '<p>Привет, мир!</p>'; ?>
+ </body>
+</html>
