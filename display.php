bcd48492ccc23d2a27afdcf04372ab1ef2ca5a9e
Volodymyr Kolesnikov <114644546+Fewch1@users.noreply.github.com>
2022-11-20 19:43:42 +0100

    Update and rename display.php to second.php

--- display.php
+++
@@ -1,6 +0,0 @@
-<?php
-$name = $_POST["firstname"];
-$surname = $_POST["lastname"];
-$usercity = $_POST["user_city"];
-echo "Hello, <b>".$name . " " . $surname . " " . $usercity . "</b> .";
-?>
---
+++ second.php
@@ -0,0 +1,23 @@
+<?php
+
+$name = $_POST["firstname"];
+$surname = $_POST["lastname"];
+$usercity = $_POST["user_city"];
+$number = $_POST["number"];
+$email = $_POST["email"];
+$sex = $_POST["sex"];
+$date = $_POST["date"];
+$file = fopen("text.txt","w") or die("Unable to open file!");
+
+echo "Hello, <b>".$name . " " . $surname . " " . "from " . $usercity . "</b> .";
+
+fwrite($file, $name);
+fwrite($file, $surname);
+fwrite($file, $usercity);
+fwrite($file, $number);
+fwrite($file, $email);
+fwrite($file, $sex);
+fwrite($file, $date);
+
+fclose($file1);
+?>
