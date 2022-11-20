ee6e792ba263b793b9df85c2d9d5e6fb0e2ebd32
Volodymyr Kolesnikov <114644546+Fewch1@users.noreply.github.com>
2022-11-20 22:12:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,25 +3,26 @@
 <title>NoTitle</title>
 <meta charset="utf-8">
 <style>
+
 body{
   background-image: url("image.jpg");
 }
 </style>
 </head>
 <body>
-<h2><b>Contact form:</b></h2>
+<h2 align="center"><b>Contact form:</b></h2>
 <form action="second.php" method="POST">
 
-<p>Your name: <input type="text" name="firstname" value="Firstname" /> <input type="text" name="lastname" value="Lastname" /> </p>
-<p>Your number: <input type="number" name="number" value="+421xxxxxxxxx" /> </p>
-<p>Your Email: <input type="email" name="email" value="example@gmail.com"/></p>  
+<p align="center">Your name: <input type="text" name="firstname" value="Firstname" /> <input type="text" name="lastname" value="Lastname" /> </p>
+<p align="center">Your number: <input type="number" name="number" value="+421xxxxxxxxx" /> </p>
+<p align="center">Your Email: <input type="email" name="email" value="example@gmail.com"/></p>  
 
-<input id="radio_1" type="radio" name="rad" value="Male" />
+<p align="center"><input id="radio_1" type="radio" name="rad" value="Male" />
 <label for="radio_1">Muž</label><br />
 <input id="radio_2" type="radio" name="rad" value="Female" />
-<label for="radio_2">Žena</label><br />
+<label for="radio_2">Žena</label><br /></p>
 
-<p>You are from:<select name="user_city">
+<p align="center">You are from:<select name="user_city">
   <optgroup label="Slovakia">
     <option value="Bratislava">Bratislava</option>
     <option value="Košice">Košice</option>
@@ -33,12 +34,10 @@ body{
   </optgroup>
 </select></p>
   
-<p>Date of Birthday <input type="date" name="date"/></p>
+<p align="center">Date of Birthday <input type="date" name="date"/></p>
   
 
-
-<p><input type="reset" name="reset" value="Clear" /></p>
-<input type="submit" value="Submit form">
+<p align="center"><input type="reset" name="reset" value="Clear" /> <input type="submit" value="Submit form"></p>
 
 </form>
 </body>
