adf9f109b406a17f1ab1bfbc5d6e2521ed2f86eb
Volodymyr Kolesnikov <114644546+Fewch1@users.noreply.github.com>
2022-12-21 18:05:00 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-<title>NoTitle</title>
+<title>MusicBox</title>
 <meta charset="utf-8">
 <style>
 
@@ -10,31 +10,26 @@ body{
 </style>
 </head>
 <body>
-<h2 align="center"><b>Contact form:</b></h2>
-<form action="second.php" method="POST">
+<h2 align="center"><b>Music Box</b></h2>
+<form action="second.php" method="POST"> 
 
-<p align="center">Your name: <input type="text" name="firstname" value="Firstname" /> <input type="text" name="lastname" value="Lastname" /> </p>
-<p align="center">Your number: <input type="number" name="number" value="+421xxxxxxxxx" /> </p>
-<p align="center">Your Email: <input type="email" name="email" value="example@gmail.com"/></p>  
+<p align="center"><input id="radio_1" type="radio" name="mode" value="first" />
+  <label for="radio_1">Songs</label><br />
+  <input id="radio_2" type="radio" name="mode" value="second" />
+  <label for="radio_2">With Remote</label><br /></p>
 
-<p align="center"><input id="radio_1" type="radio" name="rad" value="Male" />
-<label for="radio_1">Muž</label><br />
-<input id="radio_2" type="radio" name="rad" value="Female" />
-<label for="radio_2">Žena</label><br /></p>
-
-<p align="center">You are from:<select name="user_city">
-  <optgroup label="Slovakia">
-    <option value="Bratislava">Bratislava</option>
-    <option value="Košice">Košice</option>
+<p align="center">Choose a song:<select name="song">
+  <optgroup label="Games">
+    <option value="Mario">Mario</option>
   </optgroup>
-  <optgroup label="Ukraina">
-    <option value="Kyjev">Kyjev</option>
-    <option value="Lviv">Lviv</option>
-    <option value="Odesa">Odesa</option>
+  <optgroup label="Films">
+    <option value="Star_Wars">Star Wars</option>
+  </optgroup>
+  <optgroup label="Songs">
+    <option value="USSR">USSR</option>
+    <option value="New_Year">New Year</option>
   </optgroup>
 </select></p>
-  
-<p align="center">Date of Birthday <input type="date" name="date"/></p>
   
 
 <p align="center"><input type="reset" name="reset" value="Clear" /> <input type="submit" value="Submit form"></p>
